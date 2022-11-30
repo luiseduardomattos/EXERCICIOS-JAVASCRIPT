b0b5d4283c5a4fd79df2f853704f79aaa0d98e95
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2022-11-29 21:56:34 -0300

    EXERCICIOS

---
+++ EXERCICIO006/app.js
@@ -0,0 +1,6 @@
+function Multiplicacao(){
+let n1 = document.getElementById("numero1").Value
+let n2 = document.getElementById("numero2").Value
+let Multiplicacao = parseFloat(n1) * parseFloat(n2)
+document.getElementById("Resposta").innerHTML = Multiplicacao
+}
